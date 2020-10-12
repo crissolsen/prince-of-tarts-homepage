0d80ce41ce3d24ac21160a58dc38452a0622e351
Christopher <chris@homecode.co.za>
2020-10-12 11:45:08 +0200

    Bootstrap carousel

---
+++ _data/cloudinaryWidget.js
@@ -0,0 +1,35 @@
+// const myGallery = cloudinary.galleryWidget({
+//   container: "#my-gallery",
+//   cloudName: "dvwsiufhl",
+//   displayProps: {
+//     topOffset: 110, // to account for the menu element at the top of this documentation page
+//   },
+//   aspectRatio: "16:9",
+//   mediaAssets: [
+//     {
+//       tag: "pots",
+//       transformation: {
+//         fetch_format: "auto",
+//         quality: "auto",
+//         crop: "fill",
+//       },
+//     },
+//   ],
+//   carouselStyle: "thumbnails",
+//   carouselLocation: "left",
+//   viewportBreakpoints: [
+//     {
+//       breakpoint: 600,
+//       carouselStyle: "thumbnails",
+//       carouselLocation: "bottom",
+//     },
+//     {
+//       breakpoint: 300,
+//       carouselStyle: "indicators",
+//       carouselLocation: "bottom",
+//       navigation: "always",
+//     },
+//   ],
+// });
+
+// myGallery.render();
--- _data/fetchimages.js
+++ _data/fetchimages.js
@@ -6,8 +6,24 @@ module.exports = async function () {
     const response = await axios.get(
       process.env.CLOUDINARY_URL 
     );
-    return response.data
+      //Once we get all the image data, push it to array with only the URLS. Use that with parseURL function to add in transformations.
+      let allData = response.data.resources
+      let allURLS = []
+      for(let i=0;i<allData.length;i++) {
+        allURLS.push(transformedURL(allData[i].url))
+      }
+     return allURLS
   } catch (error) {
     console.error(error);
   }
 };
+
+  //Takes current URL and adds in any transformation for all of them from Cloudinary. In this case, q_auto and f_auto
+function transformedURL(url) {
+  let baseURL = "http://res.cloudinary.com/dvwsiufhl/image/upload/"
+  let optimisations = "q_auto,f_auto/"
+  let optimisedURLEnd = baseURL.concat(optimisations)
+  let endOfURL = url.slice(baseURL.length);
+  let returnedURL = optimisedURLEnd.concat(endOfURL)
+  return returnedURL
+}
